25d325fad8006787081768752a4a59ab4bae3f7d
Titouan Guionneau <titouan.guionneau@epita.fr>
2023-06-23 18:34:01 +0200

    feat: map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResultListComponent } from './result-list/result-list.component';
 import { ResultComponent } from './result/result.component';
+import { MapResultsComponent } from './map-results/map-results.component';
+import { LeafletModule} from "@asymmetrik/ngx-leaflet";
 
 @NgModule({
   declarations: [
     AppComponent,
     ResultListComponent,
-    ResultComponent
+    ResultComponent,
+    MapResultsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LeafletModule
   ],
   providers: [],
   bootstrap: [AppComponent]
