5e19ba1a6eccec0ea3e42c80f0a2851f1c571f57
Titouan Guionneau <titouan.guionneau@epita.fr>
2023-06-23 19:42:47 +0200

    feat: map implementation

--- src/app/results-handler/results-handler.component.ts
+++ src/app/results-handler/results-handler.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {GasStationData} from "../gas-station-data";
 import {GasStationsGetterService} from "../gas-stations-getter.service";
+import {MapResultsComponent} from "../map-results/map-results.component";
 
 @Component({
   selector: 'app-results-handler',
@@ -8,6 +9,7 @@ import {GasStationsGetterService} from "../gas-stations-getter.service";
   styleUrls: ['./results-handler.component.css']
 })
 export class ResultsHandlerComponent implements OnInit{
+  @ViewChild(MapResultsComponent) mapResultsComponent: MapResultsComponent | undefined;
   @Input() address: string = '';
   @Input() fuelType: string = '';
   gasService: GasStationsGetterService;
@@ -27,7 +29,14 @@ export class ResultsHandlerComponent implements OnInit{
       const position = this.gasService.formatAddressData(data);
       this.gasService.requestStationData(this.fuelType, position).subscribe((data: any) => {
         this.gasResults = this.gasService.formatStationData(data, this.fuelType);
-        console.log(this.gasResults)
+        // Create map
+        this.mapResultsComponent?.setView(position.latitude, position.longitude, 12);
+        for (const gasResult of this.gasResults) {
+          this.mapResultsComponent?.addMarker(gasResult.latitude, gasResult.longitude, gasResult.address);
+        }
+
+        // Create list of results
+        // TODO
       });
     });
   }
